7dc7f93971136eada455729562ed1ebd64f9b269
kyl <bandit@li98-183.members.linode.com>
2023-04-28 15:16:41 -0400

    remove my_write function used to debug

--- lib/write.c
+++ lib/write.c
@@ -3,17 +3,3 @@
 #include "../include/sys/types.h"
 
 _syscall3(int,write,int,fd,const char *,buf,off_t,count);
-
-ssize_t my_write(int fd, const void *buf, size_t count) {
-	ssize_t ret;
-
-	asm volatile (
-        "syscall"
-        : "=a" (ret)
-        //                 EDI      RSI       RDX
-        : "0"(__NR_write), "D"(fd), "S"(buf), "d"(count)
-        : "rcx", "r11", "memory"
-	);
-
-	return ret;
-}
